104cb3ff568e5174a0220ec22a198cc2b30742d0
Ethan Georgel <egeorgel@c2r11p3.42nice.fr>
2025-06-02 14:09:58 +0200

    changed compiler to gcc

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-char*f="#include <stdio.h>%cchar*f=%c%s%c;%cvoid	call(){printf(f,10,34,f,34,10,10,10,10,92,110,34,10,10,10,10,10);}%cint main(){call();%c/*%c	10 = %c%c and 34 = %c%c*/%c}%c/*%c	comment%c*/";
-void	call(){printf(f,10,34,f,34,10,10,10,10,92,110,34,10,10,10,10,10);}
-int main(){call();
+char*f="#include <stdio.h>%cchar*f=%c%s%c;%cvoid	call(){printf(f,10,34,f,34,10,10,10,10,10,92,110,34,92,10,10,10,10,10);}%cint main(){%c	call();%c/*%c	10 = %c%c, 34 = %c, 92 = %c, 110 = n%c*/%c}%c/*%c	comment%c*/";
+void	call(){printf(f,10,34,f,34,10,10,10,10,10,92,110,34,92,10,10,10,10,10);}
+int main(){
+	call();
 /*
-	10 = \n and 34 = "
+	10 = \n, 34 = ", 92 = \, 110 = n
 */
 }
 /*
--- C/Sully.c
+++ C/Sully.c
@@ -11,7 +11,7 @@
 #endif
 #define NAME "Sully_%c.c"
 #define NEWNAME "Sully_%c"
-#define STR "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#include <sys/wait.h>%c#include <stdlib.h>%c#define I %c%c#ifdef COMP%c #define X I-1%c#else%c #define X I%c#endif%c#define NAME %cSully_%%c.c%c%c#define NEWNAME %cSully_%%c%c%c#define STR %c%s%c%cvoid parent(pid_t pid)%c{%c	pid_t ret = waitpid(pid, NULL, 0);%c	if (ret < 0) {exit(1);}%c}%cvoid	child(char **params)%c{%c	execve(params[0], params, NULL);%c	exit(0);%c}%cvoid process(char **params)%c{%c	pid_t pid = fork();%c	if (pid < 0) {exit(1);}%c	else if (pid == 0) {child(params);}%c	else if (pid > 0) {parent(pid);}%c}%cint main()%c{%c	if (X < 0) {exit(0);}%c	char name[20];%c	sprintf(name, NAME, X+48);%c	char newname[20];%c	sprintf(newname, NEWNAME, X+48);%c	char *clang[] = {%c/bin/clang%c, %c-o%c, newname, %c-DCOMP=1%c, name, NULL};%c	char *exec[] = {newname, NULL};%c	FILE *file = fopen(name, %cw%c);%c	if (!file) {return 1;}%c	fprintf(file,STR,10,10,10,10,10,X+48,10,10,10,10,10,10,34,34,10,34,34,10,34,STR,34,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,34,34,34,34,34,34,10,10,34,34,10,10,10,10,10,10);%c	fclose(file);%c	process(clang);%c	process(exec);%c}"
+#define STR "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#include <sys/wait.h>%c#include <stdlib.h>%c#define I %c%c#ifdef COMP%c #define X I-1%c#else%c #define X I%c#endif%c#define NAME %cSully_%%c.c%c%c#define NEWNAME %cSully_%%c%c%c#define STR %c%s%c%cvoid parent(pid_t pid)%c{%c	pid_t ret = waitpid(pid, NULL, 0);%c	if (ret < 0) {exit(1);}%c}%cvoid	child(char **params)%c{%c	execve(params[0], params, NULL);%c	exit(0);%c}%cvoid process(char **params)%c{%c	pid_t pid = fork();%c	if (pid < 0) {exit(1);}%c	else if (pid == 0) {child(params);}%c	else if (pid > 0) {parent(pid);}%c}%cint main()%c{%c	if (X < 0) {exit(0);}%c	char name[20];%c	sprintf(name, NAME, X+48);%c	char newname[20];%c	sprintf(newname, NEWNAME, X+48);%c	char *gcc[] = {%c/bin/gcc%c, %c-o%c, newname, %c-DCOMP=1%c, name, NULL};%c	char *exec[] = {newname, NULL};%c	FILE *file = fopen(name, %cw%c);%c	if (!file) {return 1;}%c	fprintf(file,STR,10,10,10,10,10,X+48,10,10,10,10,10,10,34,34,10,34,34,10,34,STR,34,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,34,34,34,34,34,34,10,10,34,34,10,10,10,10,10,10);%c	fclose(file);%c	process(gcc);%c	process(exec);%c}"
 void parent(pid_t pid)
 {
 	pid_t ret = waitpid(pid, NULL, 0);
@@ -36,12 +36,12 @@ int main()
 	sprintf(name, NAME, X+48);
 	char newname[20];
 	sprintf(newname, NEWNAME, X+48);
-	char *clang[] = {"/bin/clang", "-o", newname, "-DCOMP=1", name, NULL};
+	char *gcc[] = {"/bin/gcc", "-o", newname, "-DCOMP=1", name, NULL};
 	char *exec[] = {newname, NULL};
 	FILE *file = fopen(name, "w");
 	if (!file) {return 1;}
 	fprintf(file,STR,10,10,10,10,10,X+48,10,10,10,10,10,10,34,34,10,34,34,10,34,STR,34,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,34,34,34,34,34,34,10,10,34,34,10,10,10,10,10,10);
 	fclose(file);
-	process(clang);
+	process(gcc);
 	process(exec);
 }
